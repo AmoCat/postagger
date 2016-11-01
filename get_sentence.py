b63b3b9045b3c732d4b64dab2b5408398c56d4c5
AmoCat <jqzhu@ir.hit.edu.cn>
2016-11-01 17:40:04 +0800

    train完好

--- get_sentence.py
+++ get_sentence.py
@@ -8,45 +8,50 @@ import os
 import cPickle as pkl
 
 class Data_processor(object):
-    def __init__(self, embedding_size, embedding_name):
+    def __init__(self, embedding_size, embedding_name, mode = "train_"):
         self.embedding_size = embedding_size
         self.embedding_name = embedding_name
-        self.words, self.labels = self.read_sentences()
-        self.dic = self.word_frequency_dic()
-        self.preprocess(self.dic) #首尾加<B><E>,替换UNK
-        self.embedding_nums = self.word_embedding()
-        self.convert_words_to_int(embedding_name, self.embedding_nums)
-        #os.system("./word2vec -train fsentences.txt -output word_embedding -size 100")
-        self.label_nums = self.hash_label(self.labels)  #得到标签数量
-        print "embedding_nums: ",self.embedding_nums,", label_nums: ",self.label_nums
+        if mode == "train_":
+            self.words, self.labels = self.read_sentences()
+            self.dic = self.word_frequency_dic()
+            self.preprocess(self.dic) #首尾加<B><E>,替换UNK
+            self.embedding_nums = self.word_embedding()
+            self.convert_words_to_int(embedding_name, self.embedding_nums)
+            #os.system("./word2vec -train fsentences.txt -output word_embedding -size 100")
+            self.label_nums = self.hash_label(self.labels)  #得到标签数量
+            print "embedding_nums: ",self.embedding_nums,", label_nums: ",self.label_nums
+        elif mode == "test_":
+            pass
 
-    def read_sentences(self, out_filename='./sentences.txt'):
+    def read_sentences(self, out_filename='sentences.txt', mode = "train_"):
         "words: a dict of words"
         "labels: a dict of labels"
         "read data and convert it to sentences in ./sentences.txt"
-        train_dataset = read_dataset('./penn.train.pos.gz')
-        out = open(out_filename,'w')
+        f_name = "penn." + mode.rstrip('_') + ".pos.gz"
+        train_dataset = read_dataset(f_name)
+        out = open(mode + out_filename,'w')
         words = ' '.join([' '.join(tup[0]) for tup in train_dataset]).split()
         labels = ' '.join([' '.join(tup[1]) for tup in train_dataset]).split()
         out.write('\n'.join([' '.join(tup[0]) for tup in train_dataset]))
         out.close()
         return words,labels
 
-    def word_frequency_dic(self):
+    def word_frequency_dic(self, mode = "train_"):
         "get a dict of word frequencies"
         c = Counter()
-        train_dataset = read_dataset('./penn.train.pos.gz')
+        f_name = "penn." + mode.rstrip('_') + ".pos.gz"
+        train_dataset = read_dataset(f_name)
         #统计每个词出现的次数
         for t in train_dataset:
             for word in t[0]:
                 c[word] += 1
         return c
 
-    def preprocess(self, dic,in_filename = 'sentences.txt',out_filename = 'fsentences.txt'):
+    def preprocess(self, dic,in_filename = 'sentences.txt', out_filename = 'fsentences.txt', mode = "train_"):
         "Replace the low-frequency word(<5) as \'UNK\'"
         "add \'<B>\' at the beginning of sentences,\'<>\' at the end of sentences"
         "write the processed sentences into ./fsentences.txt"
-        with open(in_filename) as sentences,open(out_filename,'w') as out:
+        with open(mode + in_filename) as sentences,open(mode + out_filename,'w') as out:
             for line in sentences:
                 words = line.split()
                 s = "<B> "
@@ -59,16 +64,16 @@ class Data_processor(object):
                 s.rstrip()
                 out.write(s+'\n')
 
-    def word_embedding(self, sentences = "fsentences.txt", output = "word_embedding", size = "50"):
+    def word_embedding(self, sentences = "fsentences.txt", output = "word_embedding", size = "50", mode = "train_"):
         '''
         get word embedding
         if no error , return nums of embeddings
         '''
-        res = os.system("./word2vec -train " + sentences + " -output origin_embedding -size " + size)
+        res = os.system("./word2vec -train " + mode + sentences + " -output origin_embedding -size " + size)
         with open("origin_embedding", 'r') as f:
             data = f.read().strip().split('\n')
             first_line = data[0].split()
-        with open(output, 'w') as f:
+        with open(mode + output, 'w') as f:
             for line in data[1:]:
                 f.write(line + '\n')
         return int(first_line[0])
@@ -84,52 +89,38 @@ class Data_processor(object):
         return count,dictionary,reverse_dictionary
     '''
 
-    def convert_words_to_int(self, embedding_name, nums, size = 50):
+    def convert_words_to_int(self, embedding_name, nums, size = 50, mode = "train_"):
         dictionary = {} #dict(word, int)
         embedding = np.ndarray([nums,size],np.float32 ) #dict(word, embedding)
-        with open(embedding_name) as f:
+        with open(mode + embedding_name) as f:
             for line in f:
                 words = line.split()
-                ifdictionary[words[0]] = len(dictionary)
+                dictionary[words[0]] = len(dictionary)
                 for i in range(size):
                     embedding[len(dictionary)-1][i] = np.float32(words[i+1])
             reverse_dictionary = dict(zip(dictionary.values(),dictionary.keys()))
-            pkl.dump(embedding, open('embedding_dict', 'w'))
-            pkl.dump(dictionary, open('word_to_int_dict', 'w'))
-            pkl.dump(reverse_dictionary, open('int_to_word_dict', 'w'))
+            pkl.dump(embedding, open(mode + 'embedding_dict', 'w'))
+            pkl.dump(dictionary, open(mode + 'word_to_int_dict', 'w'))
+            pkl.dump(reverse_dictionary, open(mode + 'int_to_word_dict', 'w'))
 
-    def hash_label(self, labels):
+    def hash_label(self, labels, mode = "train_"):
         dictionary = dict()
         count = list()
         count.extend(collections.Counter(labels).most_common())
         for label, _ in count:
             dictionary[label] = len(dictionary)
         reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-        pkl.dump(dictionary, open('lab_to_int_dic', 'w'))
-        pkl.dump(reverse_dictionary, open('int_to_lab_dic', 'w'))
+        pkl.dump(dictionary, open(mode + 'lab_to_int_dic', 'w'))
+        pkl.dump(reverse_dictionary, open(mode + 'int_to_lab_dic', 'w'))
         return len(count)
 
-    def gen_label_vector(self, label_size, label, lab_to_int_name = 'lab_to_int_dict'):
+    def gen_label_vector(self, label_size, label, lab_to_int_name = 'lab_to_int_dict', mode = "train_"):
         vector = np.zeros([label_size], dtype = np.float32)
-        lab_to_int = pkl.load(open(lab_to_int_name, 'r'))
+        lab_to_int = pkl.load(open(mode + lab_to_int_name, 'r'))
         index = lab_to_int[label]
         vector[index] = 1.0
         return vector
 
-    def gen_data(self, in_filename='./fsentences.txt'):
-        emb_dic = pkl.load(open('emb_dic'))
-        lab_dic = pkl.load(open('lab_dic'))
-        embedding = pkl.load(open('embedding'))
-        word_window = np.ndarray([None,300],dtype = np.float32)
-        cnt = 0
-        with open(in_filename,'r') as f:
-            for line in f:
-                words = line.split()
-                for i in range[1:(len(words)-2)]:
-                    pre = embedding[emb_dic[words[i-1]]]
-                    now = embedding[emb_dic[words[i]]]
-                    fu = embedding[emb_dic[words[i+1]]]
-
 if __name__ == '__main__':
-    data_processor = Data_processor(50, "word_embedding")
+    data_processor = Data_processor(50, "word_embedding", "train")
 
--- mlp.py
+++ mlp.py
@@ -4,74 +4,6 @@ import numpy as np
 from dataset import read_dataset
 import cPickle as pkl
 
-class Batch(object):
-    def gen_window(self, int_words_list, int_labels_list):
-        embedding = pkl.load(open("embedding_dict", 'r')).tolist()
-        word_to_int = pkl.load(open("word_to_int_dict", 'r'))
-        int_start = word_to_int['<B>']
-        int_end = word_to_int['<E>']
-        the_int = word_to_int['the']
-        words_windows = []
-        labels_windows = []
-        for sentence in int_words_list:
-            for i in range(0,len(sentence)):
-                window = []
-                window.extend(embedding[i-1] if i > 0 else embedding[int_start])
-                window.extend(embedding[i])
-                window.extend(embedding[i+1] if i < len(sentence) - 1 else embedding[int_end])
-                words_windows.append(window)
-
-        for sentence in int_labels_list:
-            for index in sentence[0:]:
-                window = []
-                for id in range(45):
-                    window.append(0.0)
-                window[i] = 1.0
-                labels_windows.append(window)
-
-        x = np.array(words_windows, np.float32)
-        y_ = np.array(labels_windows, np.float32)
-        l = len(x)
-        perm = np.arange(l)
-        np.random.shuffle(perm)
-        x = x[perm]
-        y_ = y_[perm]
-        return x, y_, l
-
-    def get_data(self):
-        '''
-        获得两个列表：
-        int_words_list:包含每个句子的词对应int的列表
-        int_labels_list:包含每个句子的label对应的int的列表
-        例：
-        int_words_list[0]:
-        [57, 37, 306, 1098, 1771, 7, 16, 18, 1, 19, 27, 544,\
-        12, 9135, 10629, 82, 16, 1, 1, 7651, 3, 1, 11, 1, 784,\
-        2, 19, 1, 90, 8160, 80, 2, 3, 875, 7, 1, 2, 1932, 24,\
-         8595, 1, 2, 31, 10310, 1530, 8, 1, 1, 6]
-        int_labels_list[1]:
-        [1, 3, 2, 8, 0, 1, 23, 3, 0, 24, 1, 2, 20, 2, 2, 35, 23,\
-         15, 5, 18, 3, 0, 1, 2, 2, 6, 24, 0, 12, 5, 34, 6, 3, 0,\
-          1, 2, 6, 15, 1, 2, 2, 6, 10, 9, 15, 13, 2, 2, 7]
-        '''
-        train_data_set = read_dataset("./penn.train.pos.gz")
-        word_to_int = pkl.load(open("word_to_int_dict", 'r'))
-        lab_to_int = pkl.load(open("lab_to_int_dic", 'r'))
-        int_words_list = []
-        int_labels_list = []
-        for t in train_data_set:
-            words = t[0]
-            labels = t[1]
-            int_words = []
-            for w in words:
-                if word_to_int.has_key(w):
-                    int_words.append(word_to_int[w])
-                else:
-                    int_words.append(word_to_int['UNK'])
-            int_labels = [lab_to_int[l] for l in labels]
-            int_words_list.append(int_words)
-            int_labels_list.append(int_labels)
-        return int_words_list,int_labels_list
 
 class MLP(object):
     def __init__(self, embedding_size, label_size):
@@ -87,8 +19,8 @@ class MLP(object):
         self.n_hidden_2 = 17
         self.words, self.labs = self.get_data()
         self.nums_data = len(self.words)
-        self.x, self.y_ , self.window_nums = self.gen_window(self.words, self.labs)
-        print "nums of windows: ", self.window_nums
+        self.train_x, self.train_y_ , self.train_window_nums = self.gen_window(self.words, self.labs)
+        print "nums of windows: ", self.train_window_nums
         self.multilayer_perception(self.embedding_size, self.label_size)
 
     def multilayer_perception(self, embedding_size, label_size):
@@ -119,7 +51,7 @@ class MLP(object):
 
         with tf.Session() as sess:
             sess.run(init)
-            total_batch = self.window_nums/self.window_size
+            total_batch = self.train_window_nums/self.batch_size
             for i in range(total_batch):
                 batch_x, batch_y = self.next_batch(self.batch_size)
                 optimizer.run(feed_dict = {x:batch_x, y_:batch_y})
@@ -140,19 +72,18 @@ class MLP(object):
     def next_batch(self, batch_size):
         start = self._index_in_epoch
         self._index_in_epoch += batch_size
-        if self._index_in_epoch > self.window_nums:
+        if self._index_in_epoch > self.train_window_nums:
             end = self.window_nums - 1
         else:
             end = self._index_in_epoch
-        return self.x[start:end],self.y_[start:end]
+        return self.train_x[start:end],self.train_y_[start:end]
 
 
-    def gen_window(self, int_words_list, int_labels_list):
-        embedding = pkl.load(open("embedding_dict", 'r')).tolist()
-        word_to_int = pkl.load(open("word_to_int_dict", 'r'))
+    def gen_window(self, int_words_list, int_labels_list, mode = "train_"):
+        embedding = pkl.load(open(mode + "embedding_dict", 'r')).tolist()
+        word_to_int = pkl.load(open(mode + "word_to_int_dict", 'r'))
         int_start = word_to_int['<B>']
         int_end = word_to_int['<E>']
-        the_int = word_to_int['the']
         words_windows = []
         labels_windows = []
         for sentence in int_words_list:
@@ -180,7 +111,7 @@ class MLP(object):
         y_ = y_[perm]
         return x, y_, l
 
-    def get_data(self):
+    def get_data(self, mode = "train_"):
         '''
         获得两个列表：
         int_words_list:包含每个句子的词对应int的列表
@@ -196,12 +127,13 @@ class MLP(object):
          15, 5, 18, 3, 0, 1, 2, 2, 6, 24, 0, 12, 5, 34, 6, 3, 0,\
           1, 2, 6, 15, 1, 2, 2, 6, 10, 9, 15, 13, 2, 2, 7]
         '''
-        train_data_set = read_dataset("./penn.train.pos.gz")
-        word_to_int = pkl.load(open("word_to_int_dict", 'r'))
-        lab_to_int = pkl.load(open("lab_to_int_dic", 'r'))
+        f_name = "penn." + mode.rstrip('_') + ".pos.gz"
+        data_set = read_dataset(f_name)
+        word_to_int = pkl.load(open(mode + "word_to_int_dict", 'r'))
+        lab_to_int = pkl.load(open(mode + "lab_to_int_dic", 'r'))
         int_words_list = []
         int_labels_list = []
-        for t in train_data_set:
+        for t in data_set:
             words = t[0]
             labels = t[1]
             int_words = []
